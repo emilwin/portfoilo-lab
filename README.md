f1c342bb320f7749f59461c2045def6b09299a67
Emil Winiszewski <emil.winiszewski@gmail.com>
2022-07-14 17:25:10 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Aplikacja służy do oddawania rzeczy potrzebującym.
+
+Jak zacząć?
+Zarejestruj się w dziale "Załóż konto"
+Zaloguj się w dziale "Zaloguj się"
+W przypadku jakichkolwiek pytań lub próby kontaktu ze mną wyślij mi mail na adres
+emil.winiszewski@gmail.com
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './sass/App.scss';
+import { BrowserRouter as Router, Routes, Route, Link, Redirect } from "react-router-dom";
+
+ import Home from "./components/Home";
+// import LogIn from "./components/LogIn";
+// import Register from "./components/Register";
+// import GiveItems from "./components/GiveItems";
+// import AboutUs from "./components/AboutUs";
+
+const App = () => <div>Odam w Dobre ręce</div>;
+
+export default App;
